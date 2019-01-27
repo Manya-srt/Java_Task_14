483d2c8475b844c69d55c76e1d3e930665ebc9cc
Mariya27 <mariya.akulova.91@inbox.ru>
2019-01-28 00:37:37 +0600

    Commit

--- src/Task_14.java
+++ src/Task_14.java
@@ -20,9 +20,9 @@ public class Task_14 {
             for (int i = array.length-1; i >= 1; i--){
                 for (int j = 0; j < i; j++){
                     if(array[j] > array[j+1]){
-                        int tmp = array[j];
+                        int dummy = array[j];
                         array[j] = array[j+1];
-                        array[j+1] = tmp;
+                        array[j+1] = dummy;
                     }
                 }
             }
